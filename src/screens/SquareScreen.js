27adf9aa080d6db7bb85494732f36c9df4b7dae3
OwaisIbrahim <owaisibrahim99@gmail.com>
2019-11-10 00:59:10 +0500

    6.20 Dynamic Color Changer Box using Reducer - Community Convension - Optional

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,21 +6,21 @@ const COUNTER_INCREMENT = 15;
 
 const reducer = (state, action) => {
     // state = { red:number, green:number, blue:number };
-    // action === { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
+    // action === { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15 }
 
-    switch(action.colorToChange) {
-        case 'red':
-            return state.red + action.amount > 255 || state.red + action.amount < 0
+    switch(action.type) {
+        case 'change_red':
+            return state.red + action.payload > 255 || state.red + action.payload < 0
             ? state
-            : { ...state, red: state.red + action.amount };
-        case 'green':
-            return state.green + action.amount > 255 || state.green + action.amount < 0
+            : { ...state, red: state.red + action.payload };
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload < 0
                 ? state
-                : { ...state, green: state.green + action.amount };
-        case 'blue':
-            return state.blue + action.amount > 255 || state.blue + action.amount < 0
+                : { ...state, green: state.green + action.payload };
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0
                 ? state
-                : { ...state, blue: state.blue + action.amount };
+                : { ...state, blue: state.blue + action.payload };
         default:
             return state;
     }
@@ -33,16 +33,16 @@ const SquareScreen = () => {
     return (
         <View>
             <ColorCounter 
-                onIncrease={ () => dispatch({ colorToChange: 'red', amount: COUNTER_INCREMENT }) }
-                onDecrease={ () => dispatch({ colorToChange: 'red', amount: -1 * COUNTER_INCREMENT }) }
+                onIncrease={ () => dispatch({ type: 'change_red', payload: COUNTER_INCREMENT }) }
+                onDecrease={ () => dispatch({ type: 'change_red', payload: -1 * COUNTER_INCREMENT }) }
                 color="Red"/>
             <ColorCounter 
-                onIncrease={ () => dispatch({ colorToChange: 'green', amount: COUNTER_INCREMENT }) }
-                onDecrease={ () => dispatch({ colorToChange: 'green', amount: -1 * COUNTER_INCREMENT }) }
+                onIncrease={ () => dispatch({ type: 'change_green', payload: COUNTER_INCREMENT }) }
+                onDecrease={ () => dispatch({ type: 'change_green', payload: -1 * COUNTER_INCREMENT }) }
                 color="Green"/>
             <ColorCounter 
-                onIncrease={ () => dispatch({ colorToChange: 'blue', amount: COUNTER_INCREMENT }) }
-                onDecrease={ () => dispatch({ colorToChange: 'blue', amount: -1 * COUNTER_INCREMENT }) }
+                onIncrease={ () => dispatch({ type: 'change_blue', payload: COUNTER_INCREMENT }) }
+                onDecrease={ () => dispatch({ type: 'change_blue', payload: -1 * COUNTER_INCREMENT }) }
                 color="Blue" />
             <View style = {{ 
                 width: 100,
