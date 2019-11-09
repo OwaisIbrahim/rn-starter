99909a92fc7e8ce798cb5ee31e24d31233727028
OwaisIbrahim <owaisibrahim99@gmail.com>
2019-11-10 01:20:24 +0500

    6.21 Excercise on Reducer: To change Counter App useState into useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Button, StyleSheet, Text } from 'react-native';
 
+const COUNTER_INCREMENT = 1;
+
+const reducer = (state, action) => {
+  // state === { count: number }
+  // action === { type: 'increment' || 'decrement', payload: 1 }
+
+  switch(action.type) {
+    case 'increment':
+      return { ...state, count: state.count + action.payload};
+    case 'decrement':
+      return { ...state, count: state.count - action.payload};
+    default:
+      return state;
+  }
+}
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
+    const { count } = state;
     return (
         <View>
             <Button
               title='Increase'
               onPress={() => {
-                setCounter(counter + 1);
+                dispatch({type: 'increment', payload: COUNTER_INCREMENT});
               }}  
             />
             
             <Button
               title='Decrease'
               onPress={() => {
-                setCounter(counter - 1);
+                dispatch({type: 'decrement', payload: COUNTER_INCREMENT});
               }}  
             />
-            <Text>Current Counter: {counter}</Text>
+            <Text>Current Counter: {count}</Text>
         </View>
     );
 }
